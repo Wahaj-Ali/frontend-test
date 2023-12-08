0f04882f2aa9ea3a2c8ae795a3cf6df049f9b233
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-12-08 14:02:30 +0500

    Create and add Useful-links section.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
+import GetInTouch from '../../Components/GetInTouch/GetInTouch'
 
 const Home = () => {
   return (
     <div className='home'>
         <Header />
+        <GetInTouch />
     </div>
   )
 }
