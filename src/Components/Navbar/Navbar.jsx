bbfaef6401728435bf3c6b2cb9a4fb6a5581cfb2
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-12-08 16:25:54 +0500

    Add nav menu for smaller screen sizes.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import { FaAngleRight } from "react-icons/fa6";
 
 const Navbar = () => {
+
+  const navLinks = [ 
+    { name: 'Resources', url: '/'},
+    { url: '/', name: 'Provisional Funding Info' },
+    { url: '/contact_us', name: 'Contact Us' },
+  ].filter(Boolean);
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 820);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 820);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+
+    if (!isMobileMenuOpen) {
+      document.body.classList.add('mobile-menu-active');
+    } else {
+      document.body.classList.remove('mobile-menu-active');
+    }
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+    document.body.classList.remove('mobile-menu-active');
+  };
+
   return (
     <nav className='navbar'>
       <div className="nav-top">
@@ -14,10 +52,22 @@ const Navbar = () => {
         </div>
       </div>
       <div className="nav-down">
-        <Link to='/'><h1 className='logo'>LOGO</h1></Link>
-        <a className='nav-link' href="">Resources</a>
-        <a className='nav-link' href="">Provisional Funding Info</a>
-        <a className='nav-link' href="">Contact Us</a>
+        <Link to='/'  onClick={closeMobileMenu}><h1 className='logo'>LOGO</h1></Link>
+        <div className="mobile-menu-button" onClick={toggleMobileMenu}>
+          <span className="menu-icon">&#9776;</span>
+        </div>
+        <ul className={`nav-list ${isMobileMenuOpen ? 'open' : ''}`}>
+          {navLinks.map(({ url, name }) => (
+            <li
+              key={name}
+              className='nav-item'
+              onClick={closeMobileMenu} 
+            >
+              <Link to={url}>{name}</Link>
+              {isMobileView && <FaAngleRight />}
+            </li>
+          ))}
+        </ul>
       </div>
       <hr className="gradient-line" />
     </nav>
