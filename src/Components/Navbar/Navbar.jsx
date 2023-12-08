5502b70ae6b619133886b5185f987b06a93e3769
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-12-08 15:20:44 +0500

    Update hover effect.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
-        <div className="nav-top">
-            <div className="nav-top-links">
-                <p>For Canadian Health Professioanls</p>
-                <p>Safety Information</p>
-                <p>Product Monograph</p>
-                <p>English/Français</p>
-            </div>
+      <div className="nav-top">
+        <div className="nav-top-links">
+          <p>For Canadian Health Professioanls</p>
+          <p>Safety Information</p>
+          <p>Product Monograph</p>
+          <p>English/Français</p>
         </div>
-        <div className="nav-down">
-            <h1 className='logo'>LOGO</h1>
-            <a href="">Resources</a>
-            <a href="">Provisional Funding Info</a>
-            <a href="">Contact Us</a>
-        </div>
-        <hr className="gradient-line" />
+      </div>
+      <div className="nav-down">
+        <Link to='/'><h1 className='logo'>LOGO</h1></Link>
+        <a className='nav-link' href="">Resources</a>
+        <a className='nav-link' href="">Provisional Funding Info</a>
+        <a className='nav-link' href="">Contact Us</a>
+      </div>
+      <hr className="gradient-line" />
     </nav>
   )
 }
