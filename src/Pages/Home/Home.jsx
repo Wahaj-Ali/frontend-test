df9f1b206cb1bd7d46f6e934e31280b1d55d76f2
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-12-08 14:50:41 +0500

    Create & add ISI section to the homepage.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import GetInTouch from '../../Components/GetInTouch/GetInTouch'
+import ISI from '../../Components/ISI/ISI'
 
 const Home = () => {
   return (
     <div className='home'>
         <Header />
         <GetInTouch />
+        <ISI />
     </div>
   )
 }
